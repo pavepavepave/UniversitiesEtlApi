3d0a26e641661c74027f9bb0befbc5e4cc4acca0
Pavel Yakovlev <deadamag@mail.ru>
2024-03-16 19:40:02 +0300

    Added bulk data saving

--- Universities.ETL/UniversitiesDataHandler.cs
+++ Universities.ETL/UniversitiesDataHandler.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Concurrent;
+using Newtonsoft.Json;
 using Universities.DB.Models;
 using Universities.DB.Repository;
 using Universities.ETL.Models;
@@ -9,22 +10,27 @@ public class UniversitiesDataHandler
 {
     private readonly HttpClient _httpClient;
     private readonly IUniversityRepository _universityRepository;
+    private readonly ConcurrentBag<string> _countries = new()
+    {
+        "Russian Federation", "United Kingdom", "United States", "Turkey", "Portugal", "Nigeria", "Japan", "China",
+        "Brazil", "India"
+    };
+    private readonly List<UniversityDto> _universities = new List<UniversityDto>();
 
     public UniversitiesDataHandler(HttpClient httpClient, IUniversityRepository universityRepository)
     {
         _httpClient = httpClient;
         _universityRepository = universityRepository;
-
     }
 
     /// <summary>
     /// Extracts university data from a remote source based on the provided country.
     /// </summary>
-    /// <param name="county"></param>
+    /// <param name="country"></param>
     /// <returns></returns>
-    private async Task<IEnumerable<UniversityJsonModel>> ExtractData(string county)
+    private async Task<IEnumerable<UniversityJsonModel>> ExtractDataAsync(string country)
     {
-        var response = await _httpClient.GetAsync($"http://universities.hipolabs.com/search?country={county}");
+        var response = await _httpClient.GetAsync($"http://universities.hipolabs.com/search?country={country}");
         response.EnsureSuccessStatusCode();
         var json = await response.Content.ReadAsStringAsync();
         var universities = JsonConvert.DeserializeObject<List<UniversityJsonModel>>(json);
@@ -36,7 +42,6 @@ public class UniversitiesDataHandler
     /// Transforms university data from JSON format to university DTO format.
     /// </summary>
     /// <param name="universities"></param>
-    /// <param name="country"></param>
     /// <returns></returns>
     private IEnumerable<UniversityDto> TransformData(IEnumerable<UniversityJsonModel> universities)
     {
@@ -48,14 +53,33 @@ public class UniversitiesDataHandler
         });
     }
 
-    /// <summary>
-    /// Loads university data for the given country into the database.
-    /// </summary>
-    /// <param name="country"></param>
-    public async Task LoadData(string country)
+    public async Task LoadData(int maxThreads)
     {
-        var universitiesJson = await ExtractData(country);
-        var universitiesDto = TransformData(universitiesJson);
-        await _universityRepository.AddRangeAsync(universitiesDto);
+        var semaphore = new SemaphoreSlim(maxThreads);
+        var tasks = _countries.Select(country => LoadDataForCountryAsync(semaphore, country)).ToList();
+        await Task.WhenAll(tasks);
+    
+        await SaveAllData();
+    }
+    
+    private async Task LoadDataForCountryAsync(SemaphoreSlim semaphore, string country)
+    {
+        await semaphore.WaitAsync();
+        try
+        {
+            var universitiesJson = await ExtractDataAsync(country);
+            var universitiesDto = TransformData(universitiesJson);
+            _universities.AddRange(universitiesDto);
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+
+    private async Task SaveAllData()
+    {
+        await _universityRepository.AddRangeAsync(_universities);
+        _universities.Clear();
     }
 }
