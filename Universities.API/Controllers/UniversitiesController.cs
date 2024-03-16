a35b3844d358fa78efd9d5e6b797a0e99eeff74d
Pavel Yakovlev <deadamag@mail.ru>
2024-03-16 19:45:15 +0300

    Added methods for getting university and loading data

--- Universities.API/Controllers/UniversitiesController.cs
+++ Universities.API/Controllers/UniversitiesController.cs
@@ -1,17 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using Universities.DB.Repository;
+using Universities.ETL;
 
 namespace Universities.API.Controllers;
 
+[ApiController]
 [Route("api/universities")]
 public class UniversitiesController : ControllerBase
 {
-    public UniversitiesController()
+    private readonly IUniversityRepository _universityRepository;
+    private readonly UniversitiesDataHandler _universitiesDataHandler;
+
+    public UniversitiesController(IUniversityRepository universityRepository, UniversitiesDataHandler universitiesDataHandler)
     {
+        _universityRepository = universityRepository;
+        _universitiesDataHandler = universitiesDataHandler;
+    }
+   
+    [HttpGet]
+    public async Task<IActionResult> Get(string country, string universityName)
+    {
+        if (string.IsNullOrEmpty(country) || string.IsNullOrEmpty(universityName))
+            return BadRequest();
+        
+        var universities = await _universityRepository.GetByCountryAndNameAsync(country, universityName);
+        return Ok(universities);
     }
     
-    [HttpGet]
-    public async Task<object> Get()
+    [HttpPost("LoadData")]
+    public async Task<IActionResult> LoadData(int threadsCount)
     {
-        return await Task.FromResult(new object());
+        try
+        {
+            await _universitiesDataHandler.LoadData(threadsCount);
+            return Ok("Data loaded successfully.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"An error occurred while loading data: {ex.Message}");
+        }
     }
 }
